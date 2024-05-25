19d26cd2d1b8802a398f40adaeea0b5f1037cbe9
KeRN472 <gagalal471@gmail.com>
2024-05-25 17:14:27 +0300

    pivo code
    only xml

---
+++ app/src/main/java/ru/kmept/kormezhka/StartRegActivity.kt
@@ -0,0 +1,26 @@
+package ru.kmept.kormezhka
+
+import android.content.Intent.getIntent
+import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+
+
+class StartRegActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_startreg)
+
+
+
+    }
+
+    //fun SignUpClick(view: View) {
+        //var email: LinearLayout = findViewById(R.id.email)
+
+    //}
+
+
+}
+
