f7a6fb88d21bf11cf5a3167d2350762063f401a4
zzzeoq <bogomagkov.k@icloud.com>
2024-05-25 17:27:41 +0300

    Поле поиска, кнопки, начал делать recycle
    1

---
+++ app/src/main/java/ru/kmept/kormezhka/main_screen.kt
@@ -0,0 +1,11 @@
+package ru.kmept.kormezhka
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class main_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_screen_activity)
+    }
+}
