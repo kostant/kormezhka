5a4c565e3cd14f7164f8ae60e6e57c05ce9d39c4
kmept <kmept@gmail.com>
2024-06-01 14:34:38 +0300

    fadeev

--- app/src/main/java/ru/kmept/kormezhka/MainActivity.kt
+++ app/src/main/java/ru/kmept/kormezhka/MainActivity.kt
@@ -2,10 +2,12 @@ package ru.kmept.kormezhka
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
