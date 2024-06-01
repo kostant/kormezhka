a56df721471d64e9fffe99f0dddb80141b7f9575
Konstantin Antonchik <kostant12@gmail.com>
2024-06-01 13:45:50 +0300

    fix compile

--- app/src/main/java/ru/kmept/kormezhka/MainActivity.kt
+++ app/src/main/java/ru/kmept/kormezhka/MainActivity.kt
@@ -1,6 +1,7 @@
 package ru.kmept.kormezhka
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import ru.kmept.kormezhka.data.RecipesRepository
 import ru.kmept.kormezhka.data.model.Recipe
 
