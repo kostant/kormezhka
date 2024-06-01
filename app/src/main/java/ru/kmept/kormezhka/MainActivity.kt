394cebd60a8a6f749da20c31885dce617ff77db1
hasan090812 <13971397qwerty@gmail.com>
2024-06-01 14:43:37 +0300

    gg

--- app/src/main/java/ru/kmept/kormezhka/MainActivity.kt
+++ app/src/main/java/ru/kmept/kormezhka/MainActivity.kt
@@ -1,11 +1,18 @@
 package ru.kmept.kormezhka
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun gotonextpage(view: View)
+    {
+        val intent = Intent(this@MainActivity, searchclass::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/ru/kmept/kormezhka/searchclass.kt
@@ -0,0 +1,19 @@
+package ru.kmept.kormezhka
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class searchclass : AppCompatActivity () {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.search_layout)
+
+
+
+    }
+
+
+
+
+
+}
