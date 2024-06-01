d2db82c041c1fe02f488b1cd65a777bd12c83f28
sh1su19@yandex.ru <@rHDvfdZAcq3>
2024-06-01 14:47:56 +0300

    dgfdfg

--- app/src/main/java/ru/kmept/kormezhka/MainActivity.kt
+++ app/src/main/java/ru/kmept/kormezhka/MainActivity.kt
@@ -2,10 +2,44 @@ package ru.kmept.kormezhka
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import java.util.Timer
+import java.util.TimerTask
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        runTimer()
     }
+    // Запускает таймер
+    fun runTimer() {
+        val timer = Timer()
+        // указываем сколько секунд должно протикать
+        var seconds = 180
+        val task: TimerTask = object : TimerTask() {
+            override fun run() {
+                runOnUiThread {
+                    updateUiWithSeconds(seconds)
+                    seconds--
+                }
+            }
+        }
+
+        // Запускаем таймер(1000 это в миллисекундах период тика, то есть 1 секунда)
+        timer.schedule(task, 0, 1000)
+    }
+
+    fun updateUiWithSeconds(seconds: Int) {
+        val textView: TextView = findViewById(R.id.timer)
+        textView.text = "$seconds"
+
+        val minutes = seconds / 60
+        val remainSeconds = seconds % 60
+
+        val string = minutes.toString() + ":" + remainSeconds.toString()
+        textView.text = string
+
+    }
+
 }
