138d2ba59de15d96327d718a1f8fafa4e1f9ae21
Konstantin Antonchik <kostant12@gmail.com>
2024-06-01 13:11:29 +0300

    add recipes repository and users repository

--- app/src/main/java/ru/kmept/kormezhka/MainActivity.kt
+++ app/src/main/java/ru/kmept/kormezhka/MainActivity.kt
@@ -2,10 +2,20 @@ package ru.kmept.kormezhka
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.kmept.kormezhka.data.RecipesRepository
+import ru.kmept.kormezhka.data.model.Recipe
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        RecipesRepository.global.getFoodRecipes {
+            updateScreenWithRecipes(it)
+        }
+    }
+
+    fun updateScreenWithRecipes(recipes: Array<Recipe>) {
+        // Обновить экран используя рецепты из массива recipes
     }
 }
