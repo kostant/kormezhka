2bf8e75bc0bfa5c9e38c5c3c0b75915a53b3c390
zzzeoq <bogomagkov.k@icloud.com>
2024-06-01 13:31:35 +0300

    готовый экран

---
+++ app/src/main/java/ru/kmept/kormezhka/ProductsAdapter.kt
@@ -0,0 +1,36 @@
+package ru.kmept.kormezhka
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ProductsAdapter : RecyclerView.Adapter<ProductViewHolder>() {
+    var products: Array<Product> = ProductListProvider.provideProducts()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+        val view: View =
+            LayoutInflater.from(parent.context).inflate(R.layout.product_item, parent, false)
+        return ProductViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+        val product: Product = products[position]
+        val userNameTextView: TextView = holder.itemView.findViewById(R.id.user_name)
+        userNameTextView.setText(product.userName)
+
+        val productNameTextView: TextView = holder.itemView.findViewById(R.id.product_name)
+        productNameTextView.setText(product.productName)
+
+        val userImageView: ImageView = holder.itemView.findViewById(R.id.user_photo)
+        userImageView.setImageResource(product.userPhoto)
+
+        val productImageView: ImageView = holder.itemView.findViewById(R.id.product_photo)
+        productImageView.setImageResource(product.productPhoto)
+    }
+
+    override fun getItemCount(): Int {
+        return products.size
+    }
+}
--- app/src/main/java/ru/kmept/kormezhka/main_screen.kt
+++ app/src/main/java/ru/kmept/kormezhka/main_screen.kt
@@ -1,11 +1,49 @@
 package ru.kmept.kormezhka
 
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class main_screen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_screen_activity)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler)
+
+        val adapter = ProductsAdapter()
+        recyclerView.adapter = adapter
+
+        val layoutManager: RecyclerView.LayoutManager = GridLayoutManager(this, 2)
+        recyclerView.layoutManager = layoutManager
+
+        val button_add = findViewById<Button>(R.id.button_add)
+        val button_food = findViewById<Button>(R.id.button_food)
+        val button_drink = findViewById<Button>(R.id.button_drink)
+
+        setButtonColor(button_add, button_food, button_drink)
+
+    }
+    private fun setButtonColor(vararg buttons: Button) {
+        for (button in buttons) {
+            button.setOnClickListener {
+                it.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.green))
+                (it as Button).setTextColor(ContextCompat.getColor(this, R.color.text_color_light))
+                changeOtherButtonsColor(it as Button, *buttons)
+            }
+        }
+    }
+
+    private fun changeOtherButtonsColor(selectedButton: Button, vararg buttons: Button) {
+        for (button in buttons) {
+            if (button != selectedButton) {
+                button.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.gray))
+                button.setTextColor(ContextCompat.getColor(this, R.color.text_color_dark))
+            }
+        }
     }
 }
