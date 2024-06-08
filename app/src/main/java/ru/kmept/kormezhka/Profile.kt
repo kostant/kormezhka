19db15c71116b9e564fb3691d6c0c11fdf2ef0f8
Kurepin <Fkurepin1703@gmail.com>
2024-06-08 14:33:16 +0300

    Переименовали названия

---
+++ app/src/main/java/ru/kmept/kormezhka/Profile.kt
@@ -0,0 +1,10 @@
+package ru.kmept.kormezhka
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.kmept.kormezhka.R.layout.profile_main
+class Profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.profile_main)
+    }
+}
