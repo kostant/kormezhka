e2e464f60f027a5b1dbcab6f3a13e409bd2f010c
kmept <kmept@gmail.com>
2024-06-11 18:13:24 +0300

    fadeev

--- app/src/main/java/ru/kmept/kormezhka/MainActivity.kt
+++ app/src/main/java/ru/kmept/kormezhka/MainActivity.kt
@@ -37,7 +37,8 @@ class MainActivity : AppCompatActivity() {
         val noti_tab = findViewById<View>(R.id.noti_tab)
         val profile_tab = findViewById<View>(R.id.profile_tab)
         upload_tab.setOnClickListener {
-            switchFragment(FragmentSecond())
+            val intent = Intent(this, createrecip::class.java)
+            startActivity(intent)
             updateTabs(1)
         }
         home_tab.setOnClickListener {
--- app/src/main/java/ru/kmept/kormezhka/createrecip.kt
+++ app/src/main/java/ru/kmept/kormezhka/createrecip.kt
@@ -3,9 +3,9 @@ package ru.kmept.kormezhka
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class SignInActivity : AppCompatActivity() {
+class createrecip : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    setContentView(R.layout.sign_in_activity)
+        setContentView(R.layout.createrecipecnivity)
     }
 }
