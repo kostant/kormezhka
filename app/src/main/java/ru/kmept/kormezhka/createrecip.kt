24cf0db99a16736ab8fbfd280c24e99b07f08609
Студент <student@imes.local>
2024-06-15 14:36:13 +0300

    cancel

--- app/src/main/java/ru/kmept/kormezhka/createrecip.kt
+++ app/src/main/java/ru/kmept/kormezhka/createrecip.kt
@@ -1,11 +1,20 @@
 package ru.kmept.kormezhka
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
 class createrecip : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.createrecipecnivity)
+        val cancel2: TextView = findViewById(R.id.exitee)
+        cancel2.setOnClickListener {
+            finish()
+        }
+
     }
 }
