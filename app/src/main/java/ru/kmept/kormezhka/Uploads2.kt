c2d9e2077c87064b9305f5b0c09781fe260e8d2e
hasan090812 <13971397qwerty@gmail.com>
2024-06-18 18:44:49 +0300

    ingred

--- app/src/main/java/ru/kmept/kormezhka/Uploads2.kt
+++ app/src/main/java/ru/kmept/kormezhka/Uploads2.kt
@@ -1,24 +1,12 @@
 package ru.kmept.kormezhka
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 
 class Uploads2: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.uploads2)
-
-        // получаем ссылку на RecyclerView
-        val recyclerView: RecyclerView = findViewById(R.id.spisok)
-
-        // Создаем Adapter и привязываем его к RecyclerView
-        val adapter =CreateResaitAdapter ()
-        recyclerView.adapter = adapter
-
-        // Создаем LayoutManager и привязываем его к RecyclerView
-        // LinearLayoutManager значит что ячейки будут располагаться вертикально последовательно
-        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
