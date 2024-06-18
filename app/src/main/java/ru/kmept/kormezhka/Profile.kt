66dc66458bab0293927d9fbb16ef3c27b39b3399
Студент <student@imes.local>
2024-06-18 18:42:13 +0300

    добавил переход на профиль

--- app/src/main/java/ru/kmept/kormezhka/Profile.kt
+++ app/src/main/java/ru/kmept/kormezhka/Profile.kt
@@ -14,6 +14,6 @@ class Profile: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.profile, container, false)
+        return inflater.inflate(R.layout.profile_main, container, false)
     }
 }
