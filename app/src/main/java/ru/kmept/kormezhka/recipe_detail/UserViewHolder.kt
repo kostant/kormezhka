229f634cacf32682e601ce9d4553a84753d1dc56
kmept2005 <cefayoc441@pbridal.com>
2024-06-22 13:13:56 +0300

    screen

---
+++ app/src/main/java/ru/kmept/kormezhka/recipe_detail/UserViewHolder.kt
@@ -0,0 +1,16 @@
+package ru.kmept.kormezhka.recipe_detail
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import ru.kmept.kormezhka.R
+
+class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val avatarImageView: ImageView
+    val nameTextView: TextView
+    init {
+        avatarImageView = itemView.findViewById(R.id.avatarImageView)
+        nameTextView = itemView.findViewById(R.id.nameTextView)
+    }
+}
